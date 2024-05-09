178ee5e6037b612749acee696f1a0baf43757c68
amaran-th <songsy405@naver.com>
2024-05-09 16:28:30 +0900

    refactor: Init을 거치지 않고 RoomFragment를 띄우도록 수정

--- app/src/main/java/com/example/wake_up_bird/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/wake_up_bird/presentation/ui/login/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.wake_up_bird.presentation.ui.login
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wake_up_bird.databinding.LoginBinding
+import com.example.wake_up_bird.presentation.ui.init.InitActivity
+import com.example.wake_up_bird.presentation.ui.base.NavigationActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
@@ -16,6 +24,8 @@ import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 class LoginActivity: AppCompatActivity() , View.OnClickListener {
 
+    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    lateinit var upref: SharedPreferences
     private val binding by lazy { LoginBinding.inflate(layoutInflater) }
 
     private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
@@ -54,6 +64,7 @@ class LoginActivity: AppCompatActivity() , View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        upref = getSharedPreferences("upref", Activity.MODE_PRIVATE)
 
         Log.d(LoginConstants.TAG, "keyhash : ${Utility.getKeyHash(this)}")
 
@@ -72,7 +83,66 @@ class LoginActivity: AppCompatActivity() , View.OnClickListener {
     }
 
     private fun nextMainActivity() {
-        startActivity(Intent(this, InitActivity::class.java))
-        finish()
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(LoginConstants.TAG, "사용자 정보 요청 실패 $error")
+            } else if (user != null) {
+                Log.d(LoginConstants.TAG, "사용자 정보 요청 성공 : $user")
+
+                searchUser(user.id.toString(), user.kakaoAccount?.profile?.profileImageUrl, user.kakaoAccount?.profile?.nickname)
+            }
+        }
+    }
+    fun searchUser(id:String, imageUrl:String?, nickname:String?){
+        db.collection("user")
+            .whereEqualTo("sns_id", id)
+            .get()
+            .addOnSuccessListener{
+                    result ->
+                if(result.isEmpty){
+                    // 회원가입
+                    val user = mapOf(
+                        "image_url" to imageUrl,
+                        "nickname" to nickname,
+                        "room_id" to null,
+                        "sns_id" to id
+                    )
+                    val colRef: CollectionReference = db.collection("user")
+                    val docRef: Task<DocumentReference> = colRef.add(user)
+                    docRef.addOnSuccessListener { documentReference ->
+                        Log.d(LoginConstants.TAG,"Sign Up Success : " + "${documentReference.id} \n")
+                        val ueditor=upref.edit()
+                        ueditor.putString("id",documentReference.id)
+                        ueditor.apply()
+                        startActivity(Intent(this, InitActivity::class.java)) // TODO: 초기설정 화면으로 이동
+                        finish()
+                    }
+                    docRef.addOnFailureListener {
+                        Log.d(LoginConstants.TAG,"Sign Up Failure \n")
+                    }
+                }else{
+                    // 로그인
+                    val document = result.documents.get(0)
+                    val ueditor=upref.edit()
+                    ueditor.putString("id",document.id)
+                    ueditor.apply()
+                    var roomId:String? = document.getString("room_id")
+                    if(roomId!=null){
+                        // 인증방이 있을 때
+                        val ueditor=upref.edit()
+                        ueditor.putString("room_id",roomId)
+                        ueditor.apply()
+                        startActivity(Intent(this, NavigationActivity::class.java)) // TODO: 인증방으로 이동
+                        finish()
+                    }else{
+                        startActivity(Intent(this, InitActivity::class.java)) // TODO: 초기설정 화면으로 이동
+                        finish()
+                    }
+
+                }
+            }
+            .addOnFailureListener {
+                Log.d(LoginConstants.TAG,"Sign In Failure \n")
+            }
     }
 }
