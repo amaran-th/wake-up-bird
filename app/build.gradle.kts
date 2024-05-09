8b7724bdc49d2d07e077fdfb5e5da1caca5cc3f9
amaran-th <songsy405@naver.com>
2024-05-09 16:49:31 +0900

    build: CameraX 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,15 @@ dependencies {
     implementation(libs.identity.credential.android)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    // CameraX
+    implementation("androidx.camera:camera-core:1.3.0-alpha04")
+    implementation("androidx.camera:camera-camera2:1.3.0-alpha04")
+    implementation("androidx.camera:camera-lifecycle:1.3.0-alpha04")
+    implementation("androidx.camera:camera-view:1.3.0-alpha04")
+    implementation("androidx.camera:camera-mlkit-vision:1.3.0-alpha04")
+    implementation("androidx.camera:camera-extensions:1.3.0-alpha04")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
