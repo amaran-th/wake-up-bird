8228c7ef526e3fc4e460b9a2afaa08495cc1787e
pobingbin9 <ltdmswls@pusan.ac.kr>
2024-05-13 02:42:58 +0900

    CreateRoomActivity 액티비티 파일 및 create_rool.xml layout 구현

--- app/src/main/java/com/example/wake_up_bird/presentation/ui/init/InitActivity.kt
+++ app/src/main/java/com/example/wake_up_bird/presentation/ui/init/InitActivity.kt
@@ -1,15 +1,18 @@
 package com.example.wake_up_bird.presentation.ui.init
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatButton
 import com.example.wake_up_bird.R
+import com.example.wake_up_bird.presentation.ui.create_room.CreateRoomActivity
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class InitActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,55 +20,80 @@ class InitActivity: AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.init)
 
-        val searchButton = findViewById<AppCompatImageButton>(R.id.IBtn_init_keypad_1)
-        val roomButton = findViewById<AppCompatImageButton>(R.id.IBtn_init_keypad_2)
-        val codeEditText = findViewById<EditText>(R.id.ET_init_keypad_1)
+        val searchButton = findViewById<AppCompatButton>(R.id.Btn_init_1)
+        val roomButton = findViewById<AppCompatButton>(R.id.Btn_init_2)
+        val codeEditText = findViewById<EditText>(R.id.ET_init_1)
 
         val trueLayout = findViewById<FrameLayout>(R.id.Frag_code_true)
-        val passEditText = findViewById<EditText>(R.id.ET_init_keypad_2)
-        val backButton = findViewById<AppCompatImageButton>(R.id.IBtn_init_keypad_3)
-        val enterButton = findViewById<AppCompatImageButton>(R.id.IBtn_init_keypad_4)
-        val passWarning = findViewById<TextView>(R.id.TV_init_keypad_5)
+        val passEditText = findViewById<EditText>(R.id.ET_init_2)
+        val backButton = findViewById<AppCompatButton>(R.id.Btn_init_3)
+        val enterButton = findViewById<AppCompatButton>(R.id.Btn_init_4)
+        val passWarning = findViewById<TextView>(R.id.TV_init_5)
+        val roomName = findViewById<TextView>(R.id.TV_init_3)
+        val roomInfo = findViewById<TextView>(R.id.TV_init_4)
 
         val falseLayout = findViewById<FrameLayout>(R.id.Frag_code_false)
-        val checkButton = findViewById<AppCompatImageButton>(R.id.IBtn_init_keypad_5)
-
-        codeEditText.requestFocus()
-        val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        manager.showSoftInput(codeEditText, InputMethodManager.SHOW_IMPLICIT)
+        val checkButton = findViewById<AppCompatButton>(R.id.Btn_init_5)
 
         searchButton.setOnClickListener {
             val enteredCode = codeEditText.text.toString().trim()
 
-            if(enteredCode == "true") {
-                trueLayout.visibility = View.VISIBLE
-            }
-            else {
-                falseLayout.visibility = View.VISIBLE
+            if(enteredCode.isNotEmpty()) {
+                val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+                val colRef: CollectionReference = db.collection("room")
+
+                colRef.document(enteredCode).get()
+                    .addOnSuccessListener { document ->
+                        if (document.exists()) {
+                            val name = document.getString("name")
+                            val create_data = document.getString("create_data")
+                            val member_num = document.getLong("member_num")
+
+                            roomName.setText(name)
+                            roomInfo.setText(create_data + " 개설 • " + "${member_num}명" )
+
+                            trueLayout.visibility = View.VISIBLE
+                        } else {
+                            falseLayout.visibility = View.VISIBLE
+                        }
+                    }
             }
         }
 
         roomButton.setOnClickListener {
-            // 인증방 만드는 화면으로 전환하는 부분
+            roomButton.setOnClickListener {
+                val intent = Intent(this, CreateRoomActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         backButton.setOnClickListener {
             trueLayout.visibility = View.INVISIBLE
             passWarning.visibility = View.INVISIBLE
             passEditText.text.clear()
-            passEditText.setBackgroundResource(R.drawable.ic_init_search_line)
+            passEditText.setBackgroundResource(R.drawable.line_init_password)
         }
 
         enterButton.setOnClickListener {
             val enteredPass = passEditText.text.toString().trim()
 
-            if(enteredPass == "1234") {
-                // 해당 채팅방으로 입장하는 부분
-            }
-            else  {
-                passWarning.visibility = View.VISIBLE
-                passEditText.setBackgroundResource(R.drawable.ic_init_warning_line)
-            }
+            val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+            val colRef: CollectionReference = db.collection("room")
+
+            colRef.document(codeEditText.text.toString()).get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        val password = document.getString("password")
+
+                        if (enteredPass == password) {
+                            // 해당 채팅방으로 입장하는 부분
+                        } else {
+                            passWarning.visibility = View.VISIBLE
+                            passEditText.setBackgroundResource(R.drawable.line_init_warning)
+                        }
+                    }
+                }
+
         }
 
         checkButton.setOnClickListener {
---
+++ app/src/main/java/com/example/wake_up_bird/presentation/ui/login/LoginConstants.kt
@@ -0,0 +1,6 @@
+package com.example.wake_up_bird.presentation.ui.login
+
+object LoginConstants {
+    const val TAG = "Login"
+    const val APP_KEY = "d54336f4559ca2ba5a6a1a4aad14ea45"
+}
